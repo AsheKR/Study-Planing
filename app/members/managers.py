06a263cf5b38a240a277008babcdb36b89b76117
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-29 00:46:59 -0800

    User Model 및 creating_user 작성

---
+++ app/members/managers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, user_id, email, password, **extra_fields):
+        if not user_id:
+            raise ValueError('The given User ID must be set')
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(user_id=user_id, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, user_id, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(user_id, email, password, **extra_fields)
--- app/members/models.py
+++ app/members/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from members.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.CharField('유저아이디', max_length=15, unique=True, )
+    email = models.EmailField('유저이메일', unique=True, )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'user_id'
+
+    class Meta:
+        verbose_name = ('유저', )
+        verbose_name_plural = ('유저', )
