43e23a7e96df276ef97e6f019e8d99609ff1f07a
Hangang-Coin <teachmesomething2580@gmail.com>
2019-01-31 17:27:04 -0800

    TestManagedFileModel 리팩토링

--- app/repository/tests/test_models.py
+++ app/repository/tests/test_models.py
@@ -93,99 +93,63 @@ class TestManagedFileModel:
             owner=self.user
         )
 
+    def _create_managed_file(self, file_name, directory):
+        my_file = ContentFile(random.choice('abcde'))
+        self.managed_file = ManagedFile(
+            create_author=self.user,
+            name=file_name,
+            dir=directory,
+        )
+        self.managed_file.file.save('file_name', my_file)
+
+    def _create_managed_folder(self, folder_name, directory):
+        self.managed_folder = ManagedFile.objects.create(
+            create_author=self.user,
+            name=folder_name,
+            dir=directory,
+        )
+
     def test_file_field_has_correct_path(self):
         self._create_stub_user_and_repository()
-        myfile = ContentFile(random.choice('abcde'))
-        obj = ManagedFile(
-            create_author=self.user,
-            name='file_name',
-            dir=self.repo.root_folder,
-        )
-        obj.file.save('file_name', myfile)
-        assert '/user/repo/file_name' in obj.file.path
+        self._create_managed_file('file_name', self.repo.root_folder)
+        assert '/user/repo/file_name' in self.managed_file.file.path
 
     def test_create_file_also_create_a_commit(self):
         self._create_stub_user_and_repository()
-        myfile = ContentFile(random.choice('abcde'))
-        obj = ManagedFile(
-            create_author=self.user,
-            name='file_name',
-            dir = self.repo.root_folder,
-        )
-        obj.file.save('file_name', myfile)
+        self._create_managed_file('file_name', self.repo.root_folder)
         assert TrackedFileInfo.objects.count() == 1
 
     def test_dir_has_full_dir_path(self):
         self._create_stub_user_and_repository()
-        myfolder = ManagedFile.objects.create(
-            create_author=self.user,
-            name='child_folder',
-            dir=self.repo.root_folder
-        )
+        self._create_managed_folder('child_folder', self.repo.root_folder)
 
-        assert myfolder.get_parent_dir(myfolder) + myfolder.name == '/child_folder'
+        assert self.managed_folder.get_parent_dir(self.managed_folder) + self.managed_folder.name == '/child_folder'
 
     def test_get_root_dir_object(self):
         self._create_stub_user_and_repository()
-        myfolder = ManagedFile.objects.create(
-            create_author=self.user,
-            name='child_folder',
-            dir=self.repo.root_folder
-        )
-        file = ContentFile(random.choice('abcde'))
-        myfile = ManagedFile.objects.create(
-            create_author=self.user,
-            name='child_file',
-            dir=self.repo.root_folder,
-        )
-        myfile.file.save('child_file', file)
-        assert myfile.get_root_dir(myfile) == self.repo.root_folder
-        assert myfolder.get_root_dir(myfolder) == self.repo.root_folder
+        self._create_managed_folder('child_folder', self.repo.root_folder)
+        self._create_managed_file('file_name', self.repo.root_folder)
+
+        assert self.managed_file.get_root_dir(self.managed_file) == self.repo.root_folder
+        assert self.managed_folder.get_root_dir(self.managed_folder) == self.repo.root_folder
 
     def test_has_a_hash_value_for_the_file_name(self):
         self._create_stub_user_and_repository()
-        myfile = ContentFile(random.choice('abcde'))
-        obj = ManagedFile(
-            create_author=self.user,
-            name='file_name',
-            dir=self.repo.root_folder,
-        )
-        obj.file.save('file_name', myfile)
-        assert obj.file_hash == hashlib.sha1(str.encode('/file_name')).hexdigest()
+        self._create_managed_file('file_name', self.repo.root_folder)
+        assert self.managed_file.file_hash == hashlib.sha1(str.encode('/file_name')).hexdigest()
 
     def test_recursive_directory_file_has_correct_parent_dir(self):
         self._create_stub_user_and_repository()
-        first_folder = ManagedFile.objects.create(
-            create_author=self.user,
-            name='first_folder',
-            dir=self.repo.root_folder,
-        )
-        second_folder = ManagedFile.objects.create(
-            create_author=self.user,
-            name='second_folder',
-            dir=first_folder,
-        )
-        myfile = ContentFile(random.choice('abcde'))
-        obj = ManagedFile(
-            create_author=self.user,
-            name='file_name',
-            dir=second_folder,
-        )
-        obj.file.save('file_name', myfile)
-        assert '/user/repo/first_folder/second_folder/file_name' in obj.file.path
+        self._create_managed_folder('first_folder', self.repo.root_folder)
+        self._create_managed_folder('second_folder', self.managed_folder)
+        self._create_managed_file('file_name', self.managed_folder)
+        assert '/user/repo/first_folder/second_folder/file_name' in self.managed_file.file.path
 
     def test_managed_file_also_create_commit_message_with_commit_hash(self):
         self._create_stub_user_and_repository()
-        myfile = ContentFile(random.choice('abcde'))
-        obj = ManagedFile(
-            create_author=self.user,
-            name='file_name',
-            dir=self.repo.root_folder,
-        )
-        obj.file.save('file_name', myfile)
-        assert obj.trackedfileinfo.commit_set.count() == 1
-        assert obj.trackedfileinfo.commit_set.first().commit_hash
-
+        self._create_managed_file('file_name', self.repo.root_folder)
+        assert self.managed_file.trackedfileinfo.commit_set.count() == 1
+        assert self.managed_file.trackedfileinfo.commit_set.first().commit_hash
 
 
 class TestTrackedFileInfoModel:
