59bbc336151bffba0fb7ad3486de550f018256db
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-02 20:00:52 -0800

    Test: create ManagedFile로 폴더 생성 테스트 작성

--- app/repository/tests/test_apis/test_managed_file_api.py
+++ app/repository/tests/test_apis/test_managed_file_api.py
@@ -7,3 +7,8 @@ class TestManagedFileAPI(TestStubMethodMixin):
         response, token = self._create_stub_managed_file(client)
 
         assert response.status_code == 201
+
+    def test_create_folder_with_managed_file_api(self, client):
+        response, _ = self._create_stub_folder_with_managed_file(client)
+
+        assert response.status_code == 201
