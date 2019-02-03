0a8ba780623622d199ae65693e9e6fb6337901aa
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-02 20:03:46 -0800

    Test: 폴더안에 ManagedFile이 생성가능한지 테스트

--- app/repository/tests/test_apis/test_managed_file_api.py
+++ app/repository/tests/test_apis/test_managed_file_api.py
@@ -1,3 +1,7 @@
+import pytest
+from django.core.files.base import ContentFile
+from django.shortcuts import resolve_url
+
 from repository.tests.stub_mixin import TestStubMethodMixin
 
 
@@ -10,5 +14,24 @@ class TestManagedFileAPI(TestStubMethodMixin):
 
     def test_create_folder_with_managed_file_api(self, client):
         response, _ = self._create_stub_folder_with_managed_file(client)
+
+        assert response.status_code == 201
+
+    @pytest.mark.smoke
+    def test_create_nested_file_with_managed_file_api(self, client):
+        response, token = self._create_stub_folder_with_managed_file(client)
+
+        header = {
+            'HTTP_AUTHORIZATION': 'Token ' + token,
+        }
+
+        context = {
+            'repository': '1',
+            'dir': '2',
+            'name': 'managed_file',
+            'file': ContentFile('Hello World!'),
+        }
+
+        response = client.post(resolve_url('api:repository:managed_file_list_create'), data=context, **header)
 
         assert response.status_code == 201
