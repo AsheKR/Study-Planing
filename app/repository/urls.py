68e7bde7ac6eacf052d5b5e52c52ee0dd7d2e8dc
Hangang-Coin <teachmesomething2580@gmail.com>
2019-02-02 21:20:15 -0800

    Refactor: repository, file url 분리

--- app/repository/urls.py
+++ app/repository/urls.py
@@ -1,15 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 
 from repository.apis import views
 
 app_name = 'repository'
+
+file_patterns = ([
+    path('file/',
+         views.ManagedFileCreateGenericAPIView.as_view(),
+         name='managed_file_create'),
+    path('<int:repository_pk>/directory/<int:dir_pk>/file/<int:file_pk>/',
+         views.ManagedFileRetrieveUpdateDestroyGenericAPIView.as_view(),
+         name='managed_file_create'),
+])
 
 urlpatterns = [
     path('', views.RepositoryListCreateGenericAPIView.as_view(), name='repository_list_create'),
     path('<int:pk>/',
          views.RepositoryRetrieveUpdateDestroyGenericAPIView.as_view(),
          name='repository_retrieve_update_destroy'),
-    path('<int:repository_pk>/directory/<int:dir_pk>/file/',
-         views.ManagedFileCreateGenericAPIView.as_view(),
-         name='managed_file_create')
+    path('<int:repository_pk>/directory/<int:dir_pk>/', include(file_patterns))
 ]
